82880804bf2b89e457ecdc4773c7be772c3e5581
Yassine <ge87won@mytum.de>
2024-10-10 18:54:18 +0200

    Pushed with git fast

--- src/components/product-overview/index.tsx
+++ src/components/product-overview/index.tsx
@@ -7,12 +7,10 @@ const OverviewAndPerformance = () => {
   return (
     <Box background="#FCFCFC" p="24px" mb="24px">
       <Text fontSize="lg" fontWeight="bold" mb="4">
-        Overview
+        DCgen Governance Core ($DCA)
       </Text>
       <Text fontSize="md" color="gray.600" mb="8">
-        DCG is a structured product that tracks the performance of the leading governance tokens on Ethereum. 
-        There are no fees to the product besides potential gas fees. The product is built upon conducted research and uses 
-        SetProtocol&apos;s smart contract infrastructure. Buy on Arbitrum.
+        DCG is a structured product that tracks the performance of the leading governance tokens on Ethereum. There are no fees to the product besides potential gas fees. The product is built upon conducted research and uses SetProtocol&apos;s smart contract infrastructure. Buy on Arbitrum.
       </Text>
       <ChartSection />
       <InfoSection />
